4db1845fb51403e28104664a5e7d5f447ea0a139
Ramon <amonwj.s@outlook.com`>
2025-05-03 10:59:12 -0300

    Implement Java application with basic structure and CI workflow

--- teste1.java
+++ teste1.java
@@ -1,4 +1,9 @@
+public class teste1 {
 
-public static void main(String[] args){
-    system.out.println("Hello World! " );
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello World");
+        teste2 t = new teste2();
+        System.out.println(t.sum(1, 2));
+    }
 }
---
+++ teste2.java
@@ -0,0 +1,6 @@
+public class teste2 {
+    
+    protected Integer sum(int a, int b) {
+        return a + b;
+    }
+}
